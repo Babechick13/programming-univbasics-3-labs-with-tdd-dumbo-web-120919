47853c5fdf4583be527fda1d470ddeb17ce36985
Jessica Natovich <jessicanatovich@yahoo.com>
2019-11-26 21:46:51 +0000

    Automatically backed up by Learn

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -5,13 +5,13 @@ describe "./calculator.rb" do
   it "contains a local variable called first_number that is assigned to a number" do
     first_number = get_variable_from_file('./calculator.rb', "first_number")
 
-    expect(first_number).to be_an(Integer).or be_a(Float)
+    expect(first_number).to be_an(2).or be_a(2.3)
   end
 
   it "contains a local variable called second_number that is assigned to a number that isn't 0" do
     second_number = get_variable_from_file('./calculator.rb', "second_number")
 
-    expect(second_number).to be_an(Integer).or be_a(Float)
+    expect(second_number).to be_an().or be_a()
     expect(second_number).not_to equal(0)
   end
 
@@ -20,7 +20,7 @@ describe "./calculator.rb" do
     second_number = get_variable_from_file('./calculator.rb', "second_number")
     sum = get_variable_from_file('./calculator.rb', "sum")
 
-    expect(sum).to eq(first_number+second_number)
+    expect().to eq(first_number+second_number)
   end
 
   it "contains a local variable called difference that is assigned to the result of subtracting first_number and second_number" do
@@ -28,7 +28,7 @@ describe "./calculator.rb" do
     second_number = get_variable_from_file('./calculator.rb', "second_number")
     difference = get_variable_from_file('./calculator.rb', "difference")
 
-    expect(difference).to eq(first_number-second_number)
+    expect().to eq(first_number-second_number)
   end
 
   it "contains a local variable called product that is assigned to the result of multiplying first_number and second_number" do
@@ -36,7 +36,7 @@ describe "./calculator.rb" do
     second_number = get_variable_from_file('./calculator.rb', "second_number")
     product = get_variable_from_file('./calculator.rb', "product")
 
-    expect(product).to eq(first_number*second_number)
+    expect().to eq(first_number*second_number)
   end
 
   it "contains a local variable called quotient that is assigned to the result of dividing first_number by second_number" do
@@ -44,6 +44,6 @@ describe "./calculator.rb" do
     second_number = get_variable_from_file('./calculator.rb', "second_number")
     quotient = get_variable_from_file('./calculator.rb', "quotient")
 
-    expect(quotient).to eq(first_number/second_number)
+    expect().to eq(first_number/second_number)
   end
 end
